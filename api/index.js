2993bbe12de7f6c008f98a457d7f909a55eb537a
Jaeho Kim <101899896+jaehokimdev@users.noreply.github.com>
2023-08-06 12:52:42 -0600

    added bring image and address from petsmart homepage

--- api/index.js
+++ api/index.js
@@ -16,12 +16,14 @@ const getHTML = async (keyword) => {
 const parsing = async (keyword) => {
   const html = await getHTML(keyword);
   const $ = cheerio.load(html.data);
-  const $itemList = $(".product-tile");
+  const $itemList = $(".name-link");
   let items = [];
   $itemList.each((idx, node) => {
     items.push({
       title: $(node).find(".product-name > h3").text(),
       price: $(node).find(".price-regular").text(),
+      image: $(node).find(".product-image > img").attr("src"),
+      address: "https://www.petsmart.ca" + $(node).attr("href"),
     });
   });
   console.log(items);
