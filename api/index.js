d28ab14a7e6750d46b0b9dd873f464f31acc65aa
Jaeho Kim <101899896+jaehokimdev@users.noreply.github.com>
2023-08-06 23:49:08 -0600

    added petvalue homepage

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const getHTML = async (keyword) => {
+const getPetSmart = async (keyword) => {
   try {
     return await axios.get(
       "https://www.petsmart.ca/search/?q=" +
@@ -12,16 +12,40 @@ const getHTML = async (keyword) => {
     console.error(e);
   }
 };
+const getPetValue = async (keyword) => {
+  try {
+    return await axios.get(
+      "https://www.petvalu.ca/search?query=" + encodeURI(keyword)
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-const parsing = async (keyword) => {
-  const html = await getHTML(keyword);
+const getCanadianTire = async (keyword) => {
+  try {
+    return await axios.get(
+      "https://www.canadiantire.ca/en/search-results.html?q=" +
+        encodeURI(keyword)
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const parsingPetSmart = async (keyword) => {
+  const html = await getPetSmart(keyword);
   const $ = cheerio.load(html.data);
   const $itemList = $(".name-link");
   let items = [];
   $itemList.each((idx, node) => {
     items.push({
       title: $(node).find(".product-name > h3").text(),
-      price: $(node).find(".price-regular").text(),
+      price: $(node)
+        .find(".price-regular")
+        .text()
+        .replaceAll("\n", "")
+        .replaceAll("Old Price", ""),
       image: $(node).find(".product-image > img").attr("src"),
       address: "https://www.petsmart.ca" + $(node).attr("href"),
     });
@@ -29,4 +53,47 @@ const parsing = async (keyword) => {
   console.log(items);
 };
 
-parsing("arm and hammer");
+const parsingPetValue = async (keyword) => {
+  const html = await getPetValue(keyword);
+  const $ = cheerio.load(html.data);
+  const $itemList = $(".DynamicProductListItem__Product");
+  let items = [];
+  $itemList.each((idx, node) => {
+    console.log(node);
+    items.push({
+      title: $(node).find(".ProductResultName").text(),
+      price: $(node).find(".ProductResultPrice__ProductPrice").text(),
+      image:
+        "https://www.petvalu.ca" +
+        $(node).find(".Img__Wrapper > img").attr("src"),
+      address:
+        "https://www.petvalu.ca" +
+        $(node).find(".ProductResultImage").attr("href"),
+    });
+  });
+  console.log(items);
+};
+
+const parsingCanadianTire = async (keyword) => {
+  const html = await getCanadianTire(keyword);
+  const $ = cheerio.load(html.data);
+  const $itemList = $(".nl-product__content");
+  let items = [];
+  $itemList.each((idx, node) => {
+    items.push({
+      title: $(node).find(".nl-product-card__title").text(),
+      price: $(node).find(".ProductResultPrice__ProductPrice").text(),
+      image:
+        "https://www.petvalu.ca" +
+        $(node).find(".Img__Wrapper > img").attr("src"),
+      address:
+        "https://www.petvalu.ca" +
+        $(node).find(".ProductResultImage").attr("href"),
+    });
+  });
+  console.log(items);
+};
+
+// parsingPetSmart("arm and hammer");
+// parsingPetValue("cat food");
+parsingCanadianTire("cat food");
